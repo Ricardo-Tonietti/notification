48acb92fa8cae20a651022259e6e9dfbb961e54e
Ricardo Tonietti <rictonietti@gmail.com>
2022-04-27 14:13:43 -0300

    notification

---
+++ src/main/java/com/ead/notification/repositories/NotificationRepository.java
@@ -0,0 +1,20 @@
+package com.ead.notification.repositories;
+
+import com.ead.notification.enums.NotificationStatus;
+import com.ead.notification.models.NotificationModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<NotificationModel, UUID> {
+
+
+    Page<NotificationModel> findAllByUserIdAndNotificationStatus(UUID userId, NotificationStatus notificationStatus, Pageable pageable);
+
+    Optional<NotificationModel> findByNotificationIdAndUserId(UUID notificationId, UUID userId);
+}

b98968080746d8329797b9080c00d2d2b03cec60
Ricardo Tonietti <rictonietti@gmail.com>
2022-05-06 14:13:07 -0300

    Spring Security Server

--- src/main/java/com/ead/notification/controllers/UserNotificationController.java
+++ src/main/java/com/ead/notification/controllers/UserNotificationController.java
@@ -9,6 +9,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,13 +26,15 @@ public class UserNotificationController {
     public UserNotificationController(NotificationService notificationService) {
         this.notificationService = notificationService;
     }
-
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/users/{userId}/notifications")
     public ResponseEntity<Page<NotificationModel>> getAllNotificationByUser(@PathVariable (value = "userId")UUID userId,
-                                            @PageableDefault(page = 0, size = 10, sort = "notificationId", direction =  Sort.Direction.ASC) Pageable pageable){
-        return ResponseEntity.status(HttpStatus.OK).body(notificationService.findAllNotificationByUser(userId, pageable));
-    }
+                                            @PageableDefault(page = 0, size = 10, sort = "notificationId", direction =  Sort.Direction.ASC) Pageable pageable,
+                                                                            Authentication authentication){
 
+        return ResponseEntity.status(HttpStatus.OK).body(notificationService.findAllNotificationByUser(userId, pageable ));
+    }
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @PutMapping("/users/{userId}/notifications/{notificationId}")
     public ResponseEntity<Object> updateNotification(@PathVariable(value="userId") UUID userId,
                                                      @PathVariable(value="notificationId") UUID notificationId,
